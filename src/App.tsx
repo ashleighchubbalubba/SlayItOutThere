0eaee75c8d3be835016c1eebbbcdc156aa23f7f0
ashleighchung <hichewash@gmail.com>
2020-07-17 22:52:39 -0400

    Saved Gods Thumbnails

--- src/App.tsx
+++ src/App.tsx
@@ -9,41 +9,50 @@ import ContextTestComponent from './components/context-test-component';
 import { GridRow, GridCol } from './components/Grid';
 import './App.css';
 
-const god1 = 'Bellona';
-const god2 = 'Poseidon';
-const god3 = 'Neith';
-
-  // TO DO - source devID and apiKey from ENV vars
+// TO DO - source devID and apiKey from ENV vars
 const context = createContext<SmiteAPI | null>(null);
-export const SmiteApiProvider = context.Provider; 
+export const SmiteApiProvider = context.Provider;
 export const SmiteApiConsumer = context.Consumer;
 
 function App() {
-
-  const [smiteApi, setSmiteApi] = useState(new SmiteAPI('3549', '40E0A5348C974D8391B5B4AE6993B11B'));
+  const [smiteApi, setSmiteApi] = useState(
+    new SmiteAPI('3549', '40E0A5348C974D8391B5B4AE6993B11B')
+  );
   useEffect(() => {
     smiteApi.createSession();
-  }, [])
+  }, []);
+
+  const savedGods = [
+    {
+      name: 'Bellona',
+      thumbnail: '/assets/bellona.jpg',
+    },
+    {
+      name: 'Ao Kuang',
+      thumbnail: '/assets/aokuang.jpg',
+    },
+    {
+      name: 'Jing Wei',
+      thumbnail: '/assets/jingwei.jpg',
+    },
+  ];
 
   return (
-  <SmiteApiProvider value={smiteApi}>
-    <div className="App">
-      <GridRow>
-        <GridCol desktopSpan={3}>
-          <SavedGod name="Bellona" />
-          <SavedGod name="Poseidon" />
-          <SavedGod name="Neith" />
-          <SavedGod name="Jing Wei" />
-          <SavedGod name="Au Kuang" />
-          <SavedGod name="EllieBear" />
-        </GridCol>
-        <GridCol desktopSpan={9}>
-          <BuildView />
-        </GridCol>
-      </GridRow>
-      <ContextTestComponent></ContextTestComponent>
-    </div>
-  </SmiteApiProvider>
+    <SmiteApiProvider value={smiteApi}>
+      <div className="App">
+        <GridRow>
+          <GridCol desktopSpan={3}>
+            {savedGods.map(function (god) {
+              return <SavedGod name={god.name} thumbnail={god.thumbnail} />;
+            })}
+          </GridCol>
+          <GridCol desktopSpan={9}>
+            <BuildView />
+          </GridCol>
+        </GridRow>
+        <ContextTestComponent></ContextTestComponent>
+      </div>
+    </SmiteApiProvider>
   );
 }
 
---
+++ src/components/SavedGod/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+type Props = {
+  name: string;
+  thumbnail: string;
+};
+
+const SavedGod = ({ name, thumbnail }: Props) => {
+  console.log(thumbnail);
+  return (
+    <>
+      <img src={thumbnail} alt="God Thumbnail" width="150" height="150" />
+      <div className = "godName">{name}</div>
+    </>
+  );
+};
+
+export default SavedGod;
