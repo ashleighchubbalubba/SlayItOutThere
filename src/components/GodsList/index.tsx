0d3bf9600c4993a1ea6d48a36b5068adc82bd938
ashleighchung <hichewash@gmail.com>
2020-08-13 22:00:48 -0400

    Finished highlight onclick for selected God

--- src/components/GodsList/index.tsx
+++ src/components/GodsList/index.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import God from '../God';
 import './GodsList.scss';
 import { GodsListFields } from '../../types';
 
+/*
+const sampleGods = [
+    {
+      name: 'Bellona',
+      thumbnail: '/icons/bellona.jpg',
+    },
+    {
+      name: 'Jing Wei',
+      thumbnail: '/icons/jingwei.jpg',
+    },
+    {
+      name: 'Ao Kuang',
+      thumbnail: '/icons/aokuang.jpg',
+    },
+    {
+      name: 'Mulan',
+      thumbnail: '/icons/mulan.jpeg',
+    },
+  ];
+*/
+
 const GodsList = ({ godsList }: GodsListFields) => {
+  const [selectedGod, setSelectedGod] = useState(-1);
+
+  const handleSetSelectedGod = (index: number) => {
+    setSelectedGod(index);
+  };
+
   return (
     <>
       <div className="godsList">
         <div className="godsListTitle">MY SAVED GODS</div>
-        {godsList.map((god) => (
-          <God name={god.name} thumbnail={god.thumbnail} />
+        {godsList.map((god, index) => (
+          <div onClick={() => handleSetSelectedGod(index)}>
+            <God name={god.name} thumbnail={god.thumbnail} isSelectedGod={selectedGod === index}/>
+          </div>
         ))}
       </div>
     </>
