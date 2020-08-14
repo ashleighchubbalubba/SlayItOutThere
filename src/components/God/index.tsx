25388f6cfd9679c24338fb7014b27b09ad0ef238
ashleighchung <hichewash@gmail.com>
2020-08-13 21:18:57 -0400

    Changed godImgAndName to godWrapper

--- src/components/God/index.tsx
+++ src/components/God/index.tsx
@@ -5,7 +5,7 @@ import { GodFields } from '../../types';
 const God = ({ name, thumbnail }: GodFields) => {
   return (
     <>
-      <div className="godImgAndName">
+      <div className="godWrapper">
         <img src={thumbnail} alt="God Thumbnail" className = "godThumbnail"/>
         <div className="godName">{name}</div>
       </div>
