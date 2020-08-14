987b4b445eeea33f90a38615e28d5922aa292db8
ashleighchung <hichewash@gmail.com>
2020-08-13 21:09:44 -0400

    Idek

--- src/components/God/index.tsx
+++ src/components/God/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import './God.scss';
+import { GodFields } from '../../types';
 
-type Props = {
-  name: string;
-  thumbnail: string;
-};
-
-const God = ({ name, thumbnail }: Props) => {
+const God = ({ name, thumbnail }: GodFields) => {
   return (
     <>
       <div className="godImgAndName">
